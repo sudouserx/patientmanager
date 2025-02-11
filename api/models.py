70af2ba35baeca01533ceb330c3b07e68b793412
sudouserx <sudouserx@gmail.com>
2025-02-11 22:33:53 +0530

    feat: Create User , Patient, HeartRate models

---
+++ api/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .managers import CustomUserManager
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+class Patient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class HeartRate(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, db_index= True)
+    bpm = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return f"{self.patient}: {self.bpm}bpm at {self.timestamp}"
